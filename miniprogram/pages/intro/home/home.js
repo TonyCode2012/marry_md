960344848188c7a7ec05d1a0217108cd71094d5a
sheng <sheguo@cisco.com>
2019-07-09 20:34:59 +0800

    WIP: intro like page

--- miniprogram/pages/intro/home/home.js
+++ miniprogram/pages/intro/home/home.js
@@ -1,5 +1,8 @@
 // pages/intro/home/home.js
 Component({
+  options: {
+    addGlobalClass: true,
+  },
   /**
    * 组件的属性列表
    */
@@ -19,5 +22,41 @@ Component({
    */
   methods: {
 
+    gotoUserDetail(e) {
+      const user = e.currentTarget.user;
+      wx.navigateTo({
+        url: `/pages/member/home/home?user=${user}`,
+      })
+    },
+
+    // ListTouch触摸开始
+    ListTouchStart(e) {
+      this.setData({
+        ListTouchStart: e.touches[0].pageX
+      })
+    },
+
+    // ListTouch计算方向
+    ListTouchMove(e) {
+      this.setData({
+        ListTouchDirection: e.touches[0].pageX - this.data.ListTouchStart > 0 ? 'right' : 'left'
+      })
+    },
+
+    // ListTouch计算滚动
+    ListTouchEnd(e) {
+      if (this.data.ListTouchDirection == 'left') {
+        this.setData({
+          modalName: e.currentTarget.dataset.target
+        })
+      } else {
+        this.setData({
+          modalName: null
+        })
+      }
+      this.setData({
+        ListTouchDirection: null
+      })
+    },
   }
 })
