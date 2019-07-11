f730242bf6e004ace651016e8a70d4a89cf28030
sheng <sheguo@cisco.com>
2019-07-11 20:10:38 +0800

    WIP: auth page

---
+++ miniprogram/pages/authcorp/authcorp.js
@@ -0,0 +1,76 @@
+// miniprogram/pages/authcorp/authcorp.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+    imgList: [],
+    TabCur: 0,
+    scrollLeft: 0,
+    corp: ['思科', '腾讯', '百度', '...'],
+    index_corp: 0
+  },
+  tabSelect(e) {
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+      scrollLeft: (e.currentTarget.dataset.id - 1) * 60
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
