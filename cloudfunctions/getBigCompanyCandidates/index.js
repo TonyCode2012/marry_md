7a6ebeeddb7fc18a26c85e4dc6337dbc16779ece
yudayan <yudayan@cisco.com>
2019-08-26 00:18:08 +0800

    added feature big company

--- cloudfunctions/getBigCompanyCandidates/index.js
+++ cloudfunctions/getBigCompanyCandidates/index.js
@@ -2,16 +2,21 @@
 const cloud = require('wx-server-sdk')
 
 cloud.init()
+
 const db = wx.cloud.database()
-const nexus = db.collection('nexus')
+//const nexus = db.collection('nexus')
 const maxResume = 10
+
+/*
 const {
   ENV,
   OPENID,
   APPID
 } = cloud.getWXContext()
 // 云函数入口函数
+*/
 
+/*
 function getRandomArrayElements(arr, count) {
   var shuffled = arr.slice(0),
     i = arr.length,
@@ -26,48 +31,45 @@ function getRandomArrayElements(arr, count) {
   return shuffled.slice(min);
 }
 
-var getAllCompany = function(userNexus) {
+
+
+var getAllEmployees = function (userNexus) {
   if (userNexus.company == "") {
-    return Promise.resolve(null);
+    return Promise.resolve(null)
   } else {
-    return nexus.where({
-        company: _neq(""),
-        gendor: _neq(userNexus.gendor)
-      })
-      .get()
-      .then(res => {
-        if (res.data.length < 10) {
-          return res.data
-        } else {
-          return getRandomArrayElements(res.data, maxResume)
-        }
-      })
+    let allEmployeeInfo = await nexus.where({
+      company: _neq(""),
+      gendor: _neq(userNexus.gendor)
+    }).get()
+
+    if (allEmployeeInfo.data.length < 10) {
+      console.log("Here we got all employees, numbers:")
+      console.log(allEmployeeInfo.data)
+
+      return allEmployeeInfo.data
+    } else {
+      return getRandomArrayElements(res.data, maxResume)
+    }
   }
 }
+*/
+async function getUserNexus(openid) {
+  console.log("HEHEHE getUserNexus!!!")
 
-var getUserNexus = function(openId) {
-  return nexus.where({
-      _openid: OPENID
-    })
-    .get()
-    .then(res => {
-      return res.data[0];
-    })
+  let userInfo = await db.collection("nexus").where({
+    _openid: openid
+  }).get()
+
+  return userInfo.date[0]
 }
 
-exports.main = async(event, context) => {
+exports.main = async(event) => {
+  console.log("HAHAHA started!!!")
+  let openid = event.openid
+  console.log(openid)
+  let userNexus = await getUserNexus(openid)
+  console.log(userNexus)
+  //let employees = await getAllEmployees(userNexus)
+  return userNexus
 
-  let employees = await getUserNexus(OPENID).then(res => {
-    return getAllCompany(res).then(res => {
-      return res
-    })
-  })
-
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-    employees: employees
-  }
 }
