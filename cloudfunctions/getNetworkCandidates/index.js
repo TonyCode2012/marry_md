7771269a6df47013031a25f92b52d790e4b8f536
huguirong <guirong012@163.com>
2019-09-07 15:35:16 +0800

    Fix get network

--- cloudfunctions/getNetworkCandidates/index.js
+++ cloudfunctions/getNetworkCandidates/index.js
@@ -32,12 +32,32 @@ async function getUserNexus(openid) {
 
 }
 
+
+async function getUserInfo(openid) {
+  let res = await db.collection("users").where({
+    _openid: openid
+  }).get()
+  let info
+  if (res.data.length > 0) {
+    let data = res.data[0]
+    info = {
+      basic_info: data.basic_info,
+      love_info: data.love_info,
+      photos: data.photos,
+      wechat_info: data.wechat_info
+    }
+  }
+  return info
+
+}
+
+
 exports.main = async (event) => {
   let candidates = [];
   let openid = event.openid
   let userNexus = await getUserNexus(openid)
   let chain = []
-  chain.push(openid)
+  //chain.push(openid)
   let employees = await getNetworkCandidates(userNexus, 0, userNexus.gendor, openid, chain, "")
 
   return candidates
@@ -55,11 +75,19 @@ exports.main = async (event) => {
         }
         let friendNexus = await getUserNexus(i._openid)
         let tmpChain = chain
-        tmpChain.push(friendNexus._openid + ":" + i.relationship)
-
-        if (friendNexus.gendor != gendor && friendNexus.single == true) {
-
-          candidates.push(tmpChain.concat([]))
+        let chainObj = {
+          openid: friendNexus._openid,
+          relationship: i.relationship,
+          company: friendNexus.company
+        }
+        //tmpChain.push(friendNexus._openid + ":" + i.relationship + ":" + friendNexus.company)
+        tmpChain.push(chainObj)
+        if (friendNexus.gendor != gendor && friendNexus.completed == true) {
+          let userInfo = await getUserInfo(friendNexus._openid)
+          let resChain = JSON.stringify(tmpChain)
+          resChain = JSON.parse(resChain)
+          resChain.push(userInfo)
+          candidates.push(resChain.concat([]))
         }
         await getNetworkCandidates(friendNexus, level + 1, gendor, starter, tmpChain, userNexus._openid);
       }
